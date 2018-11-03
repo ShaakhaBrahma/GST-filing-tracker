33d9c5323674460cac8d10ddb4adad1b4a3cdd86
Srivatsa Bharadwaj <vatsavatsi777@protonmail.com>
2018-11-03 20:26:44 +0530

    added first login

--- login/forms.py
+++ login/forms.py
@@ -9,4 +9,3 @@ class ClientEntry(ModelForm):
 		model=Client
 		fields='__all__'
 		
-			
--- login/views.py
+++ login/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
-from .models import Client,R1a
+from .models import Client, R1a
+from django.template import RequestContext
 from .forms import ClientEntry
 
 # Create your views here.
 def index(request):
-	if request.method=="GET":
-		cform=ClientEntry()
-		#pass
-		#context={'animals':Animal.objects.all(),}
-		return render(request,'index.html',{'cform':cform})
-
-
-	if request.method=="POST":
-
-		instance = ClientEntry(request.POST)
-		instance.save()
-
-		context={'clients':Client.objects.all()}
-		return render(request,'index.html',context)
+    if request.method=="POST":
+        request_context = RequestContext(request)
+        data = Client()
+        data.gstin = request.POST.get('gstin')
+        data.phn = request.POST.get('phn')
+        data.usr = request.POST.get('usr')
+        data.passwd = request.POST.get('passwd')
+        data.mail = request.POST.get('mail')
+        data.pan = request.POST.get('pan')
+        data.save()
+        return render(request, 'index.html')
+    if request.method == "GET":
+        return render(request, 'index.html')
 		
